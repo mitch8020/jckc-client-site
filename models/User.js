593ab21e86394c9b65a33ae9d0e4044598a1f52e
JP <johnpaulmitra@yahoo.com>
2022-08-16 15:09:35 -0500

    Updated login authentication

--- models/User.js
+++ models/User.js
@@ -2,10 +2,26 @@ const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: String
+  userName: { 
+    type: String, 
+    required: true 
+  },
+  lastName: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    unique: true, 
+    required: true 
+  },
+  password: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 })
 
 // Password hash middleware.
