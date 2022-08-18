fa98cf2253b8a85a3a20e7546d8e9e993b7b50dc
JP <johnpaulmitra@yahoo.com>
2022-08-17 21:22:27 -0500

    added comments for app functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -10,17 +11,24 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+// Load Config
+dotenv.config({ path: './config/.env' })
 
-// Passport config
+// Passport Config
 require('./config/passport')(passport)
 
 connectDB()
 
 app.set('view engine', 'ejs')
+
+// Static Folder
 app.use(express.static('public'))
+
+// Body Parser
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// Logging
 app.use(logger('dev'))
 
 // Sessions
@@ -33,15 +41,24 @@ app.use(
   })
 )
   
-// Passport middleware
+// Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Flash (Error Display)
 app.use(flash())
-  
+
+// // Set Global Var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
+// Routes
 app.use('/', mainRoutes)
 app.use('/dashboard', todoRoutes)
- 
+
+// PORT Connection
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on PORT: ${process.env.PORT}. Go catch it!`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}. Go catch it!`)
 })    
