1ccc3a706141b2fb5d0ac89068f7437c6bbc33ba
JP <johnpaulmitra@yahoo.com>
2022-09-10 10:27:41 -0500

    updated routes for dashboards

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,31 @@
-// @desc    Login/Landing Page
-// @route   GET /
+// const Entry = require('../models/Entry')
+
 module.exports = {
-  getIndex: (req,res)=>{
+  // @desc    Login/Landing Page
+  // @route   GET /
+  getIndex: (req,res) => {
     res.render('index.ejs')
+  },
+
+  // @desc    Dashboard
+  // @route   GET /dashboard
+  getDashboard: async (req, res) => {
+    try {
+      if (!req.user.registrationStatus) {
+        res.redirect('/auth/acctRegistration')
+      } else {
+        res.render('dashboard.ejs')
+      }
+    } catch (error) {
+      console.error(error)
+      // res.render('error/500')
+    }
+  },
+
+  // @desc    Dashboard (Parent)
+  // @route   GET /dashboardParent/:id
+  getDashboardParent: async (req, res) => {
+    res.render('dashboard-parent.ejs')
   }
 }
+
--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,8 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 const { ensureGuest, ensureAuth } = require('../middleware/auth')
 
-// const Entry = require('../models/Entry')
-
-// @desc    Login/Landing Page
-// @route   GET /
 router.get('/', ensureGuest, homeController.getIndex)
-
-// @desc    Dashboard
-// @route   GET /dashboard
-router.get('/dashboard', ensureAuth, async (req, res) => {
-  try {
-    if (!req.user.registrationStatus) {
-      res.redirect('/auth/acctRegistration')
-    } else {
-      res.render('dashboard.ejs')
-    }
-  } catch (error) {
-    console.error(error)
-    // res.render('error/500')
-  }
-})
+router.get('/dashboard', ensureAuth, homeController.getDashboard)
+router.get('/dashboardParent/:id', ensureAuth, homeController.getDashboardParent)
 
 module.exports = router
