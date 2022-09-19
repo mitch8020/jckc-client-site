df1a18b87c911666f6bb3d17e1b67fc577809239
JP <johnpaulmitra@yahoo.com>
2022-09-19 12:15:42 -0500

    update tailwind styles and reinstall

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./views/*.{html,ejs,js}"],
+  content: ["./views/*.{html,ejs,js}", "./views/partials/*.{html,ejs,js}", "./views/partials/nav-links/*.{html,ejs,js}"],
   theme: {
     extend: {
       colors: {
