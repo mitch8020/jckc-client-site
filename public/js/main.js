2ee7b9ca4f47c4e14638588faafe15b20b4b5d34
JP <johnpaulmitra@yahoo.com>
2022-12-23 22:33:10 -0600

    add function for dropdown search menu in add guardian

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,23 @@ mobileHam.addEventListener('click', () => {
   btnHam.forEach(e => e.classList.toggle('hidden'))
   mobileMenu.classList.toggle('hidden')
 })
+
+// Search Filter Function
+function searchFilterGuardians() {
+  // Declare variables
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('guardianNameSearch');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("dropdownGuardianNames");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
